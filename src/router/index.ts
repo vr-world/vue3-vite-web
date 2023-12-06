ac1c29b503c3b5dd161ddd63ec41eea76eae015f
of1526 <xiangjiayu@bm001.com>
2023-12-06 09:11:07 +0800

    🐞 fix:更新首页路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,13 @@ import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
 
 // 静态路由表
 const routes: Array<RouteRecordRaw> = [
+  // {
+  //   // 路由重定向配置
+  //   path: '/',
+  //   redirect: '/',
+  // },
   {
-    // 路由重定向配置
     path: '/',
-    redirect: '/home',
-  },
-  {
-    path: '/home',
     meta: {
       title: '首页',
       keepAlive: false,
